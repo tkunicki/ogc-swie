ce85d5d8c4b56a208a285e9f70ca341a4c7b62a3
Jordan Walker <jiwalker@usgs.gov>
2010-10-08 16:20:03 +0000

    javaType was used instead of typeHandler, made change.
    git-svn-id: https://cida-svn.er.usgs.gov/repos/dev/usgs/nwis/wqws/branches/refactor/ogc-ie@11591 622dc1fb-2224-0410-ab72-dc85a3975e1a

--- src/test/java/gov/usgs/cida/webservices/ISODateTimeTypeHandlerTest.java
+++ src/test/java/gov/usgs/cida/webservices/ISODateTimeTypeHandlerTest.java
@@ -7,45 +7,50 @@ import org.junit.Before;
 import org.junit.After;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import static gov.usgs.webservices.ibatis.ISODateTimeTypeHandler.getISODateTimeFromSQL;
 
 public class ISODateTimeTypeHandlerTest {
-	
-	ISODateTimeTypeHandler timeHandler = null;
-    ResultSetMock rsm = null;
-    String[][] rowset = { { "2003-05-19T13:52CST", "2010-11-02T01:15EST", "1952-08-01T21:00PST"} };
-	
-	@Before
-	public void setUp() {
-		timeHandler = new ISODateTimeTypeHandler();
-		String[] columns = {"datetime"};
-		rsm = new ResultSetMock(columns, rowset);
-	}
-	
-	@After
-	public void tearDown() {
-		timeHandler = null;
-		rsm = null;
-	}
-	
-	@Test
-	public void testGetResult_ResultSetAndColumnName() throws SQLException{
-            rsm.next();
-            String output = timeHandler.getResult(rsm, "datetime");
-            assertEquals("Date not in expected format", output, "2003-05-19T13:52-06:00");
-            rsm.next();
-            output = timeHandler.getResult(rsm, "datetime");
-            assertEquals("Date not in expected format", output, "2010-11-02T01:15-05:00");
-            rsm.next();
-            output = timeHandler.getResult(rsm, "datetime");
-            assertEquals("Date not in expected format", output, "1952-08-01T21:00-08:00");
-	}
 
-	@Test (expected=UnsupportedOperationException.class)
+    ISODateTimeTypeHandler timeHandler = null;
+    ResultSetMock rsm = null;
+    String[][] rowset = {{"2003-05-19T13:52CST", "2010-11-02T01:15EST", "1952-08-01T21:00PST"}};
+
+    @Before
+    public void setUp() {
+        timeHandler = new ISODateTimeTypeHandler();
+        String[] columns = {"datetime"};
+        rsm = new ResultSetMock(columns, rowset);
+    }
+
+    @After
+    public void tearDown() {
+        timeHandler = null;
+        rsm = null;
+    }
+
+    @Test
+    public void testGetResult_ResultSetAndColumnName() throws SQLException {
+        rsm.next();
+        String output = timeHandler.getResult(rsm, "datetime");
+        assertEquals("Date not in expected format", output, "2003-05-19T13:52-06:00");
+        rsm.next();
+        output = timeHandler.getResult(rsm, "datetime");
+        assertEquals("Date not in expected format", output, "2010-11-02T01:15-05:00");
+        rsm.next();
+        output = timeHandler.getResult(rsm, "datetime");
+        assertEquals("Date not in expected format", output, "1952-08-01T21:00-08:00");
+    }
+
+    @Test
+    public void testGetResult_Static() {
+        assertEquals("Result should be something else", getISODateTimeFromSQL("2000-04-22T16:59MST"), "2000-04-22T16:59-07:00");
+    }
+    
+    @Test (expected=UnsupportedOperationException.class)
 	public void testGetResult_CallableStatement() throws SQLException {
 		timeHandler.getResult(null, 0);
-	}
-
-	// Something is stupidly wrong here, doesn't matter really
+    }
+    // Something is stupidly wrong here, doesn't matter really
 //	@Test (expected=UnsupportedOperationException.class)
 //	public void testSetParameterForUnsupported() throws SQLException {
 //		timeHandler.setParameter(null, 1, null, null);
