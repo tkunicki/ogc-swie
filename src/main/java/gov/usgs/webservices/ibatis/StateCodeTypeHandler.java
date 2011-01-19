3a96d73dbb6a128bbdcba344a17b06e52521f6ae
Laura Kranendonk <lkranendonk@usgs.gov>
2011-01-19 20:23:14 +0000

    git-svn-id: https://cida-svn.er.usgs.gov/repos/dev/usgs/ogc-ie/swie/trunk@12640 622dc1fb-2224-0410-ab72-dc85a3975e1a

---
+++ src/main/java/gov/usgs/webservices/ibatis/StateCodeTypeHandler.java
@@ -0,0 +1,18 @@
+package gov.usgs.webservices.ibatis;
+
+import org.apache.ibatis.type.JdbcType;
+import org.apache.ibatis.type.TypeHandler;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+/**
+ *
+ * @author lkranendonk
+ */
+public class StateCodeTypeHandler {
+
+}
+
+
